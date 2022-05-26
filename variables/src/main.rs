5820d5243a9b13ba54253b40586362b3fd447e1c
Rahul Shelke <srahul07@gmail.com>
2022-05-26 22:27:31 +0530

    Tuple indices

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
     let tup = (500, 6.4, 1);
 
+    println!("The first value is: {}", tup.0);
+    println!("The second value is: {}", tup.1);
+    println!("The third value is: {}", tup.2);
+
     let (x, y, z) = tup;
 
     println!("The value of x is: {}", x);
